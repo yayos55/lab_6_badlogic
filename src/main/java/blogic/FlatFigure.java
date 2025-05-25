2a03504caab325296a7ce8901217307f7aaf420b
yayos55 <yayos55@outlook.com>
2025-05-25 15:53:43 +0600

    бновил класс FlatFigure добавлено triangleArea, parallelArea, упрощен area, проверка вывнесена в ffHelp

--- src/main/java/blogic/FlatFigure.java
+++ src/main/java/blogic/FlatFigure.java
@@ -8,14 +8,18 @@ public class FlatFigure {
         this.a = a;
         this.b = b;
         this.c = c;
-        if (c<a+b && b<c+a && a<c+b)    System.out.println("ok Triangle");
-        else System.out.println("bad Triangle");
+        if (FFHelp.isTriangle(a,b,c))
+            System.out.println("ok Triangle");
+        else
+            System.out.println("bad Triangle");
     }
     public FlatFigure(double a, double b, int alfa) {
         this.a = a;
         this.b = b;
-        if (alfa==90) this.c=Math.sqrt(a*a+b*b);
-        else this.c = Math.sqrt(a*a+b*b -2*a*b*Math.cos(alfa*Math.PI/180));
+        if (alfa == 90)
+            this.c = Math.sqrt(a*a + b*b);
+        else
+            this.c = Math.sqrt(a*a + b*b - 2*a*b*Math.cos(alfa*Math.PI/180));
         this.al = alfa;
     }
     public FlatFigure(double a, int alfa) {
@@ -23,35 +27,47 @@ public class FlatFigure {
         this.b = a;
         this.c = a;
         this.d = a;
-        this.al= alfa;
-        this.bet=180-alfa;
+        this.al = alfa;
+        this.bet = 180 - alfa;
     }
     public FlatFigure(double a, double b, int alfa, int betta) {
         this.a = a;
         this.b = b;
         this.c = a;
         this.d = b;
-        if (alfa+betta==180) 
-                System.out.println("ok Parallelogram");
-        else    System.out.println("bad Parallelogram");
-        
-        this.al= alfa;
-        this.bet=betta;
-    }
-    
-    public double area() {
-        double s=0;
-        if (d==0) {
-            if (al==90) s=0.5*a*b;
-            else {
-                double p=(a+b+c)/2;
-                s=Math.sqrt(p*(p-a)*(p-b)*(p-c));
-            }
-        }
-        else if (al==90 && bet==90 && a==c && b==d) { s=a*b; }
-             else { s=a*b*Math.sin(al*Math.PI/180); }
-            
-        return s;
+        if (FFHelp.isParallelogram(alfa, betta))
+            System.out.println("ok Parallelogram");
+        else
+            System.out.println("bad Parallelogram");
+
+        this.al = alfa;
+        this.bet = betta;
     }
 
+    // приватный метод для вычисления площади треугольника
+    private double triangleArea() {
+        if (al == 90) {
+            return 0.5 * a * b;
+        } else {
+            double p = (a + b + c) / 2;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+    }
+
+    // приватный метод для вычисления площади параллелограмма
+    private double parallelArea() {
+        if (al == 90 && bet == 90 && a == c && b == d) {
+            return a * b; // прямоугольник
+        } else {
+            return a * b * Math.sin(al * Math.PI / 180);
+        }
+    }
+
+    public double area() {
+        if (d == 0) {
+            return triangleArea();
+        } else {
+            return parallelArea();
+        }
+    }
 }
